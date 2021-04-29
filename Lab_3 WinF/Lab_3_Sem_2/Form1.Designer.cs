c703191a8a4343aeecc501aa94622f8dd9c73ee6
BodyakovDeveloper <59065454+BodyakovDeveloper@users.noreply.github.com>
2021-04-30 01:02:36 +0300

    Add files via upload

---
+++ Lab_3 WinF/Lab_3_Sem_2/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Lab_3_Sem_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBoxHei = new System.Windows.Forms.TextBox();
+            this.textBoxWid = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.buttonOk.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonOk.ForeColor = System.Drawing.Color.LawnGreen;
+            this.buttonOk.Location = new System.Drawing.Point(378, 260);
+            this.buttonOk.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(126, 60);
+            this.buttonOk.TabIndex = 0;
+            this.buttonOk.TabStop = false;
+            this.buttonOk.Text = "Ok!";
+            this.buttonOk.UseVisualStyleBackColor = false;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonCancel.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonCancel.ForeColor = System.Drawing.Color.Red;
+            this.buttonCancel.Location = new System.Drawing.Point(378, 328);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(126, 41);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.TabStop = false;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = false;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBoxHei
+            // 
+            this.textBoxHei.Location = new System.Drawing.Point(12, 12);
+            this.textBoxHei.Name = "textBoxHei";
+            this.textBoxHei.Size = new System.Drawing.Size(125, 27);
+            this.textBoxHei.TabIndex = 2;
+            this.textBoxHei.Text = "Height = ";
+            // 
+            // textBoxWid
+            // 
+            this.textBoxWid.Location = new System.Drawing.Point(12, 45);
+            this.textBoxWid.Name = "textBoxWid";
+            this.textBoxWid.Size = new System.Drawing.Size(125, 27);
+            this.textBoxWid.TabIndex = 3;
+            this.textBoxWid.Text = "Width";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 589);
+            this.Controls.Add(this.textBoxWid);
+            this.Controls.Add(this.textBoxHei);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOk);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBoxHei;
+        private System.Windows.Forms.TextBox textBoxWid;
+    }
+}
+
---
+++ Lab_3 WinF/Lab_3_Sem_2/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_3_Sem_2
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Start();
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            textBoxHei.Text = "Height: ";
+            textBoxWid.Text = "Width: ";
+            textBoxHei.Text += buttonOk.Height;
+            textBoxWid.Text += buttonOk.Width;
+            if ((e.X >= buttonOk.Left - 10) && (e.X <= buttonOk.Left + buttonOk.Width + 10))
+            {
+                if (e.X >= buttonOk.Left + (buttonOk.Width / 2))
+                {
+                    buttonOk.Left = buttonOk.Left - 5;
+                }
+                else buttonOk.Left = buttonOk.Left + 5;
+                buttonOk.Width -= 1;
+                buttonOk.Height -= 1;
+            }
+            if ((e.Y >= buttonOk.Top - 10) && (e.Y <= buttonOk.Top + buttonOk.Height + 10))
+            {
+                if (e.Y >= buttonOk.Top + (buttonOk.Height / 2))
+                {
+                    buttonOk.Top = buttonOk.Top - 5;
+                }
+                else buttonOk.Top = buttonOk.Top + 5;
+                buttonOk.Width -= 1;
+                buttonOk.Height -= 1;
+            }
+            if (buttonOk.Left < 0)
+            {
+                buttonOk.Left = 50;
+            }
+            if ((buttonOk.Left + buttonOk.Width) > this.ClientSize.Width)
+            { buttonOk.Left = this.ClientSize.Width - buttonOk.Width; }
+            if (buttonOk.Top < 0)
+            {
+                buttonOk.Top = 50;
+            }
+            if ((buttonOk.Top + buttonOk.Height) > this.ClientSize.Height)
+            { buttonOk.Top = this.ClientSize.Height - buttonOk.Height; }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine(textBoxHei.Text += buttonOk.Height);
+            Console.WriteLine(textBoxWid.Text += buttonOk.Width);
+        }
+
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            while(i != 8)
+            {
+                this.Text = "Press OK!";
+                i++;
+            }
+            if (buttonOk.Width <= 0 || buttonOk.Height <= 0)
+            {
+                i = 0;
+                while (i != 8)
+                {
+                    this.Text = "You will never press OK!";
+                    i++;
+                }
+                timer1.Stop();
+            }
+        }
+    }
+}
